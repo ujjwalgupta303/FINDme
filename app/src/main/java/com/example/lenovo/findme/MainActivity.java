702f3eba44839eac2c8fd16556c280ccf9d771ee
Ujjwal Gupta <ujjwalgupta303@gmail.com>
2019-06-06 10:36:24 +0530

    integrated googleSignIn

--- app/src/main/java/com/example/lenovo/findme/MainActivity.java
+++ app/src/main/java/com/example/lenovo/findme/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.lenovo.findme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    int RC_SIGN_IN=0;
+    private ImageView google,fb;
+    GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        google=(ImageView)findViewById(R.id.googleSignIn);
+        fb=(ImageView)findViewById(R.id.fbSignIn);
+
+        GoogleSignInOptions gso= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+    mGoogleSignInClient= GoogleSignIn.getClient(this,gso);
+    google.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            signIn();
+        }
+    });
+
     }
+
+    private void signIn() {
+        Intent signInIntent=mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==RC_SIGN_IN)
+        {
+            Task<GoogleSignInAccount> task= GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask)
+    {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            startActivity(new Intent(MainActivity.this,LoginScreen.class));
+        }
+        catch (ApiException e){
+            Log.w("Google Sign In Error","signInResult:failed code"+e.getStatusCode());
+            Toast.makeText(MainActivity.this,"Failed",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onStart()
+    {
+        GoogleSignInAccount account=GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null)
+        {
+            startActivity(new Intent(MainActivity.this,LoginScreen.class));
+        }
+        super.onStart();
+
+    }
+
 }
